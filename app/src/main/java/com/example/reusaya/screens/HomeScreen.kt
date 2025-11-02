3d402e09ddb046d2bef248a15a8a59474a770753
Franco Seguel <165804915+Francoseguel@users.noreply.github.com>
2025-11-02 20:25:35 -0300

    Update HomeScreen.kt

--- app/src/main/java/com/example/reusaya/screens/HomeScreen.kt
+++ app/src/main/java/com/example/reusaya/screens/HomeScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.FilterList // Esta importación es correcta
+import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
